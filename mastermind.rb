3f9e57ac4f0c62d345fc8bf100de92572e3f11c9
akais315 <akais315@gmail.com>
2022-07-11 21:26:23 +0200

    Add rough code validation in Breaker mode

--- mastermind.rb
+++ mastermind.rb
@@ -37,7 +37,7 @@ module Mastermind
 
     def generate_code
       @code = ''
-      until @code.length == 4 && @code.split('').all? { |char| char.to_i.between?(1, 6)}
+      until @code.length == 4 && @code.split('').all? { |char| char.to_i.between?(1, 6) }
         puts 'What code will the computer try to crack (4 numbers between 1 and 6)?'
         @code = gets.chomp
       end
@@ -48,14 +48,37 @@ module Mastermind
   class Breaker
     def initialize
       puts 'Breaker initialized'
+      @attempt = 1
       @code = generate_code
       puts @code
+      @solved = false
+      solve
     end
 
     def generate_code
       code = ''
       4.times { code += rand(1..6).to_s }
       code.to_i
+    end
+
+    def solve
+      until @solved == true
+        @solved = solved?(trial)
+      end
+    end
+
+    def solved?(trial)
+      @code == trial.to_i
+    end
+
+    def trial
+      code = ''
+      until code.length == 4 && code.split('').all? { |char| char.to_i.between?(1, 6) }
+        puts "Attempt ##{@attempt}: What is the passord (4 numbers between 1 and 6) ?"
+        code = gets.chomp
+      end
+      @attempt += 1
+      code
     end
   end
 end
