dd410d4b788b7e9e7cd5b8d0ef81a665002deb2c
akais315 <akais315@gmail.com>
2022-07-11 20:07:22 +0200

    Add Maker and Breaker classes with generate_code methods

--- mastermind.rb
+++ mastermind.rb
@@ -10,11 +10,52 @@ module Mastermind
 
     def start_game
       puts 'Welcome to Mastermind !'
+      case choose_role
+      when 'b'
+        Breaker.new
+      when 'm'
+        Maker.new
+      end
+    end
+
+    def choose_role
       role = nil
       until %w[m b].include?(role)
         puts 'Will you be the Maker(m) or the Breaker(b) ?'
-        role = gets.chomp
+        role = gets.chomp.downcase
       end
+      role
+    end
+  end
+
+  # Maker game
+  class Maker
+    def initialize
+      puts 'Maker initialized'
+      generate_code
+    end
+
+    def generate_code
+      @code = ''
+      until @code.length == 4 && @code.split('').all? { |char| char.to_i.between?(1, 6)}
+        puts 'What code will the computer try to crack (4 numbers between 1 and 6)?'
+        @code = gets.chomp
+      end
+    end
+  end
+
+  # Breaker game
+  class Breaker
+    def initialize
+      puts 'Breaker initialized'
+      @code = generate_code
+      puts @code
+    end
+
+    def generate_code
+      code = ''
+      4.times { code += rand(1..6).to_s }
+      code.to_i
     end
   end
 end
